3afa548f65eb7163e63c5fdf5213b2bc0f6fa97f
Fragnaut <54330607+Fragnaut@users.noreply.github.com>
2020-06-20 13:55:06 -0300

    Update Processamento.java

--- NumbersExpandedForm/src/main/Processamento.java
+++ NumbersExpandedForm/src/main/Processamento.java
@@ -15,14 +15,15 @@ public class Processamento {
         for (int i = 0; i < arrayString.length; i++) {
             j--;
             arrayNumbers[i] = Integer.parseInt(arrayString[i]) * (int) Math.pow(10, j);
-            if (arrayNumbers[i] != 0) {
-                if (i < arrayString.length - 1) {
-                    numbers += arrayNumbers[i] + " + ";
-                } else {
-                    numbers += arrayNumbers[i];
-                }
+            if (num % 10 == 0 && i < arrayString.length - 2) {
+                numbers += arrayNumbers[i] + " + ";
+            } else if (num % 10 != 0 && i < arrayString.length - 1) {
+                numbers += arrayNumbers[i] + " + ";
+            } else {
+                numbers += arrayNumbers[i];
             }
         }
         return numbers;
-        }
+    }
+
 }
